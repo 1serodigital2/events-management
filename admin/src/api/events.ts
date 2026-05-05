b73921ec957fb4a36762ccad2724cab65c7a5e06
bijay-appy <bijaysharma122023@gmail.com>
2026-05-05 10:38:41 +0530

    get events in listing page

--- admin/src/api/events.ts
+++ admin/src/api/events.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase"; // Assuming you have your db instance exported from a firebase.js file
 
 export const queryClient = new QueryClient();
@@ -18,6 +18,29 @@ export const createEvent = async (eventData: formType) => {
     return docRef.id;
   } catch (e) {
     console.error("Error adding document: ", e);
+    // You can re-throw the error or handle it gracefully
+    throw e;
+  }
+};
+
+export interface EventType {
+  id: string;
+  name: string;
+  location: string;
+  date: string;
+  description: string;
+}
+export const getEvents = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "event"));
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as EventType[];
+
+    return data;
+  } catch (e) {
+    console.error("Error getting events: ", e);
     // You can re-throw the error or handle it gracefully
     throw e;
   }
--- admin/src/components/events/EventsList.tsx
+++ admin/src/components/events/EventsList.tsx
@@ -1,5 +1,52 @@
+// tanstack
+import { useQuery } from "@tanstack/react-query";
+
+// api
+import { getEvents } from "../../api/events";
+
 const EventsList = () => {
-  return <h1>Events List</h1>
-}
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["events"],
+    queryFn: getEvents,
+    // staleTime: 3000,
+  });
+
+  console.log("loading", isLoading);
+  console.log("isError", isError);
+  console.log("error", error);
+  console.log("data", data);
+
+  let content;
+
+  {
+    isLoading && <p>Events are loading...</p>;
+  }
+  {
+    isError && <p>{error.message}</p>;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>SL No</th>
+          <th>Event Name</th>
+          <th>location</th>
+          <th>Created at</th>
+          <th>Updated at</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data &&
+          data.map((event) => (
+            <tr>
+              <td>01</td>
+              <td>{event.name}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default EventsList;
